fa20eca1f2f5d8033935158e1da8f01535b036cd
Zeynnz <zzeynn8@gmail.com>
2025-09-29 10:38:31 +0200

    Hello world working !

--- src/main/java/org/coiffet/tp1/Tp1Application.java
+++ src/main/java/org/coiffet/tp1/Tp1Application.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Tp1Application {
 
 	public static void main(String[] args) {
@@ -14,9 +16,10 @@ public class Tp1Application {
 	}
 
 
-	@GetMapping("/helloworld")
+	@GetMapping("/bonjour")
 	public String hello() {
-		return"Hello World!";
+		return "Hello world!";
 	}
+
 
 }
