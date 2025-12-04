ac5c508be76f7a7ddf52de1881d7c1ce9fa55328
Zeynn <98015715+Zeynnz@users.noreply.github.com>
2025-12-04 09:55:36 +0100

    Modification liste de string en liste de User

--- src/main/java/org/coiffet/tp1/tokenJWT/R505UserDetailsService.java
+++ src/main/java/org/coiffet/tp1/tokenJWT/R505UserDetailsService.java
@@ -1,17 +1,16 @@
 package org.coiffet.tp1.tokenJWT;
 
-
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
-public class UserDetailsServiceR5A05 implements UserDetailsService {
-
+public class R505UserDetailsService implements UserDetailsService {
     private static final Map<String, UserDetails> users = new HashMap<>();
 
     static {
---
+++ src/main/java/org/coiffet/tp1/tokenJWT/R505WebSecurityConfigurer.java
@@ -0,0 +1,55 @@
+package org.coiffet.tp1.tokenJWT;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity
+public class R505WebSecurityConfigurer {
+
+    @Bean
+    public R505OncePerRequestFilter authenticationJwtTokenFilter() {
+        return new R505OncePerRequestFilter();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, DaoAuthenticationProvider provider, JwtAuthEntryPoint unauthorizedHandler) throws Exception {
+        http.cors(cors -> cors.disable()).csrf(csrf -> csrf.disable())
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("INSERER LES ENDPOINTS À AUTORISER ICI").permitAll()
+                        .anyRequest().authenticated());
+
+        http.authenticationProvider(provider);
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(R505UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+}
